f3c8878e8f5dd07429cd0bc412b41a18c6fd2f87
Darshan R.M <90130734+darshancpu@users.noreply.github.com>
2024-01-14 12:38:07 +0530

    dataset made

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,86 +1,9 @@
 import pandas as pd
 from glob import glob
 
-# --------------------------------------------------------------
-# Read single CSV file
-# --------------------------------------------------------------
-file = pd.read_csv('../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv')
 
 # --------------------------------------------------------------
-# List all data in data/raw/MetaMotion
-# --------------------------------------------------------------
-files = glob("../../data/raw/MetaMotion/*.csv")
-files = [f.replace('\\','/') for f in files]
-file = files[0]
-
-# --------------------------------------------------------------
-# Extract features from filename
-# --------------------------------------------------------------
-
-file_name = file.split('/')[-1]
-
-participant = file_name.split('-')[0]
-label = file_name.split('-')[1]
-category = file_name.split('-')[2].rstrip('2')
-
-df = pd.read_csv(files[0])
-df['participant'] = participant
-df['label'] = label
-df['category'] = category
-
-# --------------------------------------------------------------
-# Read all files
-# --------------------------------------------------------------
-
-acc_df = pd.DataFrame()
-gyr_df = pd.DataFrame()
-
-acc_set = 1
-gyr_set = 1
-
-for file in files:    
-    file_name = file.split('/')[-1]
-    
-    participant = file_name.split('-')[0]
-    label = file_name.split('-')[1]
-    category = file_name.split('-')[2].rstrip('_MetaWear_2019').rstrip('123')
-    
-    df = pd.read_csv(file)
-    df['participant'] = participant
-    df['label'] = label
-    df['category'] = category
-    
-    if 'Accelerometer' in file_name:
-        df['set'] = acc_set
-        acc_set += 1
-        acc_df = pd.concat([acc_df, df], axis = 0)
-    
-    if 'Gyroscope' in file_name:
-        df['set'] = gyr_set
-        gyr_set += 1
-        gyr_df = pd.concat([gyr_df, df], axis = 0)
-        
-acc_df[acc_df['set'] == 1]
-
-# --------------------------------------------------------------
-# Working with datetimes
-# --------------------------------------------------------------
-# epoch is present in milli seconds of unix time so convert it into date time and make as index 
-
-acc_df.info()
-
-pd.to_datetime(acc_df['epoch (ms)'], unit = 'ms')
-pd.to_datetime(acc_df['epoch (ms)'], unit = 'ms').dt.day
-
-acc_df.index = pd.to_datetime(acc_df['epoch (ms)'], unit = 'ms')
-gyr_df.index = pd.to_datetime(gyr_df['epoch (ms)'], unit = 'ms')
-
-# dropping unwanted datetime column
-acc_df.drop(columns = ['epoch (ms)', 'time (01:00)', 'elapsed (s)'], axis = 1, inplace = True)
-gyr_df.drop(columns = ['epoch (ms)', 'time (01:00)', 'elapsed (s)'], axis = 1, inplace = True)
-
-# --------------------------------------------------------------
-# Turn into function
+# Create accerator and gyroscope dataframe
 # --------------------------------------------------------------
 
 files = [f.replace('\\','/') for f in glob("../../data/raw/MetaMotion/*.csv")]
@@ -132,19 +55,58 @@ def read_data_from_files(files):
 
 acc_df, gyr_df = read_data_from_files(files)
 
+
 # --------------------------------------------------------------
-# Merging datasets
+# Merging datasets - to get the accelerator and gyroscope value at the time stamp
 # --------------------------------------------------------------
+
+df_merged = pd.concat([acc_df.iloc[:, :3], gyr_df], axis = 1)
+
+df_merged.columns = [
+    'acc_x', 
+    'acc_y', 
+    'acc_z', 
+    'gyr_x', 
+    'gyr_y', 
+    'gyr_z',
+    'participant',
+    'label',
+    'category',
+    'set'
+]
 
 
 # --------------------------------------------------------------
-# Resample data (frequency conversion)
+# Resample data (frequency conversion) - to make accelerator and gyroscope frequency match
 # --------------------------------------------------------------
-
 # Accelerometer:    12.500HZ
 # Gyroscope:        25.000Hz
 
+sampling = {
+    'acc_x' : 'mean',
+    'acc_y' : 'mean',
+    'acc_z' : 'mean',
+    'gyr_x' : 'mean',
+    'gyr_y' : 'mean',
+    'gyr_z' : 'mean',
+    'participant' : 'last',
+    'label' : 'last',
+    'category' : 'last',
+    'set' : 'last'
+}
+# resamping is done to take same frequency reading for both accelerator and gyroscope
+df_merged[:1000].resample(rule = '200ms').apply(sampling)
+
+# grouping dataframe by the day to apply resampling for a particular day
+days = [g for n, g in df_merged.groupby(pd.Grouper(freq = 'D'))]
+df_resampled = pd.concat([df.resample(rule = '200ms').apply(sampling).dropna() for df in days])
+
+df_resampled.info()
+df_resampled['set'] = df_resampled['set'].astype(int)
 
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+
+df_resampled.to_csv('../../data/interim/01_processed_data.csv')
+df_resampled.to_pickle('../../data/interim/01_processed_data.pkl')
